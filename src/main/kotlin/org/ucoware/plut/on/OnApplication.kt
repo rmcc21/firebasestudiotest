e0313b4e8d5589fc106ff49d0d25112f9bab0478
Ricardo Cimarelli <ricardo.cimarelli@gmail.com>
2025-04-17 11:05:27 -0300

    Initial Commit

---
+++ src/main/kotlin/org/ucoware/plut/on/OnApplication.kt
@@ -0,0 +1,11 @@
+package org.ucoware.plut.on
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnApplication
+
+fun main(args: Array<String>) {
+	runApplication<OnApplication>(*args)
+}
---
+++ src/test/kotlin/org/ucoware/plut/on/OnApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ucoware.plut.on
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
